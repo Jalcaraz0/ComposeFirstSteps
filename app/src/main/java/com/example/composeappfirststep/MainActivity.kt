ffc7e6bd6216ac495c701da9a13a0bec923a3945
Juan Alcaraz <juan5alcaraz@gmail.com>
2021-08-21 23:27:54 -0500

    Lesson 1 composable and preview function

--- app/src/main/java/com/example/composeappfirststep/MainActivity.kt
+++ app/src/main/java/com/example/composeappfirststep/MainActivity.kt
@@ -14,25 +14,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppFirstStepTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello world")
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeAppFirstStepTheme {
-        Greeting("Android")
-    }
+fun MessageCARD (name: String){
+    Text(text = "Hello $name")
+}
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCARD(name = "Juan")
 }
