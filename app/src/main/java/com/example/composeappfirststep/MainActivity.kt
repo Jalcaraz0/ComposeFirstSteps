80527ea5fede16fb29c8983c565fe7bed0bedf6d
Juan Alcaraz <juan5alcaraz@gmail.com>
2021-08-22 00:41:31 -0500

    Lesson 3 Material design

--- app/src/main/java/com/example/composeappfirststep/MainActivity.kt
+++ app/src/main/java/com/example/composeappfirststep/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.composeappfirststep
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -22,7 +24,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world")
+            ComposeAppFirstStepTheme{
+                MessageCARD(MessageInformation("Juan","Using Material design"))
+            }
         }
     }
 }
@@ -34,19 +38,33 @@ fun MessageCARD (msg:MessageInformation){
         Image(painter = painterResource(R.drawable.profile), contentDescription = "Contact profile picture",
         modifier= Modifier
             .size(40.dp)
-            .clip(CircleShape))
+            .clip(CircleShape)
+            .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape))
 
     
     Spacer(modifier = Modifier.width(8.dp))
     Column() {
-        Text(text = msg.author)
+        Text(text = msg.author, color=MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2
+        )
         Spacer(modifier = Modifier.height(4.dp))
-        Text(text = msg.body)
+        Surface(shape = MaterialTheme.shapes.medium, elevation =1.dp){
+        Text(text = msg.body,color=MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2)
+        }
         }
     }
 }
-@Preview
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard(){
-    MessageCARD(msg = MessageInformation("Juan","This is my first compose application"))
+    ComposeAppFirstStepTheme() {
+        MessageCARD(msg = MessageInformation("Manuel", "Hey this is my first application with Compose"))
+    }
 }
